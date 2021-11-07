eac69c1fb3fc9c40570a2151ccbfa47d6d184cc0
squeaker2000 <elizabeth@orchards.ws>
2021-11-07 00:44:39 -0600

    thank you page

--- Week_08/js/script.js
+++ Week_08/js/script.js
@@ -1,3 +1,7 @@
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+
+function thankYouPage() {
+    window.location.href="https://squeaker2000.github.io/WDD230/Week_08/thanks.html";
+}
