0f6165aa5f5c8bc988b2c8b5d6824265027caac9
squaker2000 <elizabeth@orchards.ws>
2021-11-11 07:53:15 -0700

    activity

--- Week_08/js/script.js
+++ Week_08/js/script.js
@@ -1,7 +1,3 @@
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
-
-function thankYouPage() {
-    window.location.href="https://squeaker2000.github.io/WDD230/Week_08/thanks.html";
-}
