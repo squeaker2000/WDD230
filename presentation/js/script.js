72ad52c8dbf35753c4c607a2071455b0c0067d81
squeaker2000 <elizabeth@orchards.ws>
2021-11-10 22:23:42 -0700

    updates on presentation

---
+++ presentation/js/script.js
@@ -0,0 +1,7 @@
+function display() {
+    DispWin = window.open('','NewWin', 'toolbar=no,status=no,width=300,height=200')
+    message = "<ul><li><b>NAME: </b>" + document.form1.yourname.value;
+    message += "<li><b>ADDRESS: </b>" + document.form1.address.value;
+    message += "<li><b>PHONE: </b>" + document.form1.phone.value + "</ul>";
+    DispWin.document.write(message);
+  }
