3744f81197228301c7cb5eb8bc39cee579d6416a
squaker2000 <elizabeth@orchards.ws>
2021-11-27 23:32:23 -0700

    updates

--- Week_11/home/js/script.js
+++ Week_11/home/js/script.js
@@ -1,39 +1,14 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const towns = jsonObject['towns'];
-    for (let i = 0; i < towns.length; i++ ) {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let image = document.createElement('img');
-        let motto = document.createElement('p');
-        let yearFounded = document.createElement('p');
-        let population = document.createElement('p');
-        let avgRain = document.createElement('p');
-        // let events = document.createElement('p');
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
 
-        h2.textContent = towns[i].name;
-        image.setAttribute('src', towns[i].photo);
-        motto.textContent = towns[i].motto;
-        yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-        population.textContent = 'Population: ' + towns[i].population;
-        avgRain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-        
-
-        card.appendChild(h2);
-        card.appendChild(image);
-        card.appendChild(motto);
-        card.appendChild(yearFounded);
-        card.appendChild(population);
-        card.appendChild(avgRain);
-        // card.appendChild(events);
-
-        document.querySelector('div.cards').appendChild(card);
-        
-    }
-  });
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
