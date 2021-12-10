4ff16ea577aa1932534426c6da63a8066b7add70
squaker2000 <elizabeth@orchards.ws>
2021-12-09 23:42:27 -0700

    changes

--- Week_13/temple/js/api.js
+++ Week_13/temple/js/api.js
@@ -1,5 +1,82 @@
-const fetchDat = async () => {
-    let key = "Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68XwZj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N"
-    const response = await fetch(" https://nathan-byui-api.herokuapp.com/temples/{temple_id}");
-    const data = await response.json();
-}
+const requestURL = 'https://nathan-byui-api.herokuapp.com/temples/{temple_id}';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+    let filteredTowns = towns;
+    if (towns.length === 7) {
+      filteredTowns = [towns[0], towns[2], towns[6]];
+    }
+
+    for (let i = 0; i < filteredTowns.length; i++) {
+      if (towns.length === 7) {
+        filteredTowns = [towns[0], towns[2], towns[6]];
+      }
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let image = document.createElement('img');
+        let motto = document.createElement('p');
+        let yearFounded = document.createElement('p');
+        let population = document.createElement('p');
+        let avgRain = document.createElement('p');
+        // let events = document.createElement('p');
+
+        h2.textContent = towns[i].name;
+        image.src = 'images/preston.jpg';
+        motto.textContent = towns[i].motto;
+        yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        population.textContent = 'Population: ' + towns[i].population;
+        avgRain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+
+        /* if (i == 0)
+        {
+        h2.textContent = towns[i].name;
+        image.src = 'images/soda_springs.jpg';
+          motto.textContent = towns[i].motto;
+          yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+          population.textContent = 'Population: ' + towns[i].population;
+          avgRain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+        }
+        // else if (i == 2)
+        {
+          h2.textContent = towns[i].name;
+          image.src = 'images/fish_haven.jpg';
+          motto.textContent = towns[i].motto;
+          yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+          population.textContent = 'Population: ' + towns[i].population;
+          avgRain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+        }
+        else if (i == 1)
+        {
+          h2.textContent = towns[i].name;
+          image.src = 'images/preston.jpg';
+          motto.textContent = towns[i].motto;
+          yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+          population.textContent = 'Population: ' + towns[i].population;
+          avgRain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+        }
+        else
+        {
+          ;
+        }*/
+          // motto.textContent = towns[i].motto;
+          //  yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+          // population.textContent = 'Population: ' + towns[i].population;
+          // avgRain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+        
+
+        card.appendChild(h2);
+        card.appendChild(image);
+        card.appendChild(motto);
+        card.appendChild(yearFounded);
+        card.appendChild(population);
+        card.appendChild(avgRain);
+        // card.appendChild(events);
+
+        document.querySelector('div.cards').appendChild(card);        
+    }
+  });
